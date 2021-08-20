5a4c215ce847c5943e138f8a18d1ea9f4b79404e
guhdalla <guhdalla011@gmail.com>
2021-08-20 13:04:50 -0300

    Projeto NAC4

---
+++ src/main/java/com/fiap/SmartCities/SmartCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.SmartCities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCitiesApplication.class, args);
+	}
+
+}
