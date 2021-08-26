ba1fede9d8b270f3cca92ed88a2d612e6d7b93a8
murilonerdx <mu-silva@outlook.com>
2021-08-26 15:33:02 -0300

    Integração com twitter

--- src/main/java/com/fiap/SmartCities/repository/VelocidadeVentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fiap.SmartCities.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.fiap.SmartCities.model.VelocidadeVento;
-
-public interface VelocidadeVentoRepository extends JpaRepository<VelocidadeVento, Long> {
-
-}
